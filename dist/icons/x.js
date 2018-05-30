c6d3e698f9790967062a7c2148ee5bae3c275b61
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-05-30 11:15:47 -0300

    Include dist folder in repo

---
+++ dist/icons/x.js
@@ -0,0 +1,60 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
+var X = function X(props) {
+  var color = props.color,
+      size = props.size,
+      otherProps = _objectWithoutProperties(props, ['color', 'size']);
+
+  return _react2.default.createElement(
+    'svg',
+    _extends({
+      width: '19',
+      height: '19',
+      viewBox: '0 0 19 19',
+      fill: 'none',
+      xmlns: 'http://www.w3.org/2000/svg'
+    }, otherProps),
+    _react2.default.createElement('rect', {
+      width: '1',
+      height: '25',
+      rx: '0.5',
+      transform: 'translate(0.00195312 0.707031) rotate(-45)'
+    }),
+    _react2.default.createElement('rect', {
+      width: '1',
+      height: '25',
+      rx: '0.5',
+      transform: 'translate(17.6777) rotate(45)'
+    })
+  );
+};
+
+X.propTypes = {
+  color: _propTypes2.default.string,
+  size: _propTypes2.default.oneOfType([_propTypes2.default.string, _propTypes2.default.number])
+};
+
+X.defaultProps = {
+  color: 'currentColor',
+  size: '24'
+};
+
+exports.default = X;
---
+++ dist/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.X = undefined;
+
+var _x = require('./icons/x');
+
+var _x2 = _interopRequireDefault(_x);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.X = _x2.default;
